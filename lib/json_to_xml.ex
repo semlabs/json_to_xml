bc098c7107693f91a5d4f7c8d5db7374f54ae087
Marian Bäuerle <m.baeuerle@semlabs.de>
2017-05-22 16:05:24 +0200

    Add file test

--- lib/json_to_xml.ex
+++ lib/json_to_xml.ex
@@ -9,19 +9,19 @@ defmodule JsonToXml do
   Converts the given json string into a xml string.
 
   ## Examples
-  iex>JsonToXml.convert(~s({ "firstname": "john", "lastname": "doe" }), :data)
+  iex>JsonToXml.convert(~s({ "firstname": "john", "lastname": "doe" }))
   "<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>\n<root>\n\t<firstname>john</firstname>\n\t<lastname>doe</lastname>\n</root>"
 
-  iex>JsonToXml.convert(~s({ "name": { "first": "john", "last": "doe" } }), :data)
+  iex>JsonToXml.convert(~s({ "name": { "first": "john", "last": "doe" } }))
   "<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>\n<root>\n\t<name>\n\t\t<first>john</first>\n\t\t<last>doe</last>\n\t</name>\n</root>"
 
-  iex>JsonToXml.convert(~s({ "person": { "name": "doe", "address": { "street": "Huffman Road", "number": "12"}} }), :data)
+  iex>JsonToXml.convert(~s({ "person": { "name": "doe", "address": { "street": "Huffman Road", "number": "12"}} }))
   "<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>\n<root>\n\t<person>\n\t\t<address>\n\t\t\t<number>12</number>\n\t\t\t<street>Huffman Road</street>\n\t\t</address>\n\t\t<name>doe</name>\n\t</person>\n</root>"
 
-  iex>JsonToXml.convert(~s({ "list": ["apple", "banana", "lemon"] }), :data)
+  iex>JsonToXml.convert(~s({ "list": ["apple", "banana", "lemon"] }))
   "<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>\n<root>\n\t<list>\n\t\t<element>apple</element>\n\t\t<element>banana</element>\n\t\t<element>lemon</element>\n\t</list>\n</root>"
   """
-  def convert(json, :data) do
+  def convert(json) do
     content =
       Poison.decode!(json)
       |> map_elements()
@@ -29,9 +29,16 @@ defmodule JsonToXml do
     XmlBuilder.doc("root", content)
   end
 
-  def convert(file, :file) do
+  @doc """
+  Converts the given json file into a xml string. 
+  
+  ## Examples
+  iex>JsonToXml.convertFile("test/fixtures/example.json")
+  "<?xml version=\\\"1.0\\\" encoding=\\\"UTF-8\\\"?>\n<root>\n\t<address>\n\t\t<number>12</number>\n\t\t<street>Huffman Road</street>\n\t</address>\n\t<array>\n\t\t<element>apple</element>\n\t\t<element>banana</element>\n\t\t<element>lemon</element>\n\t</array>\n\t<name>John Doe</name>\n</root>"
+  """
+  def convertFile(file) do
     File.read!(file)
-    |> convert(file)
+    |> convert()
   end
 
   defp map_elements(elements) do
