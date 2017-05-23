7e46d6b64e902587552b2585eb7f130f9087ae94
Marian Bäuerle <m.baeuerle@semlabs.de>
2017-05-23 10:11:01 +0200

    Fix issue where converting non string values was failing

--- test/json_to_xml_test.exs
+++ test/json_to_xml_test.exs
@@ -1,4 +1,15 @@
 defmodule JsonToXmlTest do
   use ExUnit.Case
   doctest JsonToXml
+
+  test "it can convert various data types" do
+    assert JsonToXml.convert(~s({ "foo": true })) === ~s(<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<root>\n\t<foo>true</foo>\n</root>)
+    assert JsonToXml.convert(~s({ "foo": 12 })) === ~s(<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<root>\n\t<foo>12</foo>\n</root>)
+    assert JsonToXml.convert(~s({ "foo": false })) === ~s(<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<root>\n\t<foo>false</foo>\n</root>)
+    assert JsonToXml.convert(~s({ "foo": 12.3456 })) === ~s(<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<root>\n\t<foo>12.3456</foo>\n</root>)
+  end
+
+  test "it can convert null types" do
+    assert JsonToXml.convert(~s({ "foo": null })) === ~s(<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<root>\n\t<foo/>\n</root>)
+  end
 end
