9d0b508451bebe2c3e91b9640471f6c4324be550
Marian Bäuerle <m.baeuerle@semlabs.de>
2017-08-10 13:52:14 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule JsonToXml.Mixfile do
 
   def project do
     [app: :json_to_xml,
-     version: "0.4.0",
+     version: "0.4.1",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
